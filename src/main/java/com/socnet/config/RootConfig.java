ad9cf779a6f33cc39c4c8af4bdb6b703ecff8651
ivaneh <ivanehreshi@gmail.com>
2016-11-11 12:18:01 +0200

    Remove invalid import

--- src/main/java/com/socnet/config/RootConfig.java
+++ src/main/java/com/socnet/config/RootConfig.java
@@ -3,17 +3,14 @@ package com.socnet.config;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScan.Filter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
 import org.springframework.context.support.ResourceBundleMessageSource;
-import org.springframework.context.annotation.ComponentScan.Filter;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.resource.PathResourceResolver;
-
-import sun.dc.path.PathError;
 
 @Configuration
 @ComponentScan(basePackages = "com.socnet", excludeFilters = 

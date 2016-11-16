ea9a3a15ee1856fbb88307e53d1ca935b9f9b6f0
ivaneh <ivanehreshi@gmail.com>
2016-11-17 00:12:34 +0200

    add modelmapper library to the project
    Enables easy conversion between Entity and DTO

--- src/main/java/com/socnet/config/RootConfig.java
+++ src/main/java/com/socnet/config/RootConfig.java
@@ -1,18 +1,14 @@
 package com.socnet.config;
 
-import org.springframework.context.ApplicationEventPublisher;
+import org.modelmapper.ModelMapper;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScan.Filter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
-import org.springframework.context.event.ApplicationEventMulticaster;
-import org.springframework.context.event.SimpleApplicationEventMulticaster;
 import org.springframework.context.support.ResourceBundleMessageSource;
-import org.springframework.core.task.SimpleAsyncTaskExecutor;
 import org.springframework.web.multipart.commons.CommonsMultipartResolver;
-import org.springframework.web.servlet.config.annotation.AsyncSupportConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -25,7 +21,7 @@ public class RootConfig extends WebMvcConfigurerAdapter {
 
 	private static final String PERSISTENCE_SOURCE_BASE_NAME = "properties/persistence";
 
-	@Override		
+	@Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
     	
     	registry
@@ -35,6 +31,11 @@ public class RootConfig extends WebMvcConfigurerAdapter {
     		.resourceChain(true)
     		.addResolver(new PathResourceResolver());
     }
+
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
+	}
 
 	@Bean
 	public CommonsMultipartResolver multipartResolver() {

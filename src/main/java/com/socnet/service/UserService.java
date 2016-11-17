3d590deb3e23ca2751af744253e5af1263ffda87
ruslanlazin <ruslanlazin@gmail.com>
2016-11-17 16:08:06 +0200

    change == to equals in   removeNotificationFromUser().

--- src/main/java/com/socnet/service/UserService.java
+++ src/main/java/com/socnet/service/UserService.java
@@ -14,16 +14,15 @@ import java.util.*;
 
 @Service
 public class UserService {
-
     private UsernameStorage principal;
     private UsersRepository usersRepository;
-	private ModelMapper modelMapper;
+    private ModelMapper modelMapper;
 
     @Autowired
     public UserService(UsernameStorage principal, UsersRepository usersRepository, ModelMapper modelMapper) {
         this.principal = principal;
         this.usersRepository = usersRepository;
-		this.modelMapper = modelMapper;
+        this.modelMapper = modelMapper;
     }
 
     /*    method returns User object, and saves username in sessionScope "principal"
@@ -53,7 +52,7 @@ public class UserService {
     @Transactional
     public BasicPostDto addPost(String text, String title) { //now return Post
 
-		Post post = new Post();
+        Post post = new Post();
         post.setText(text);
         post.setPostingDate(new Date());
         post.setTitle(title);
@@ -64,141 +63,138 @@ public class UserService {
 
         return modelMapper.map(user, BasicPostDto.class);
     }
-   
-   public boolean isUsernameAvailable(String username){
-	   
-	   return usersRepository.findByUsername(username) == null;
-   }
-   
-   public void save(User user){
-	   usersRepository.save(user);
-   }
-   
-   
-   //get users with with just username, firstName, lastName field
-   //it is possibly stuped solution for self reference json formatting problem
-   @Transactional
-   public Set<User> getCurrentUserFriends(){   
-	   User user = usersRepository.findByUsername(principal.getUsername());
-	   Set<User> friends = new HashSet<>();
-	   User newUser;
-	   for(User u : user.getFriends()){
-		   newUser = new User();
-		   newUser.setId(u.getId());
-		   newUser.setUsername(u.getUsername());
-		   newUser.setFirstName(u.getFirstName());
-		   newUser.setLastName(u.getLastName());
-		   friends.add(newUser);
-	   }
-	   
-	   return friends;
-   }
-   
-   @Transactional
-   public Set<User> getUserFriends(String username){
-	   
-	   User user = usersRepository.findByUsername(username);
-	   Set<User> friends = new HashSet<>();
-	   User newUser;
-	   for(User u : user.getFriends()){
-		   newUser = new User();
-		   newUser.setId(u.getId());
-		   newUser.setUsername(u.getUsername());
-		   newUser.setFirstName(u.getFirstName());
-		   newUser.setLastName(u.getLastName());
-		   friends.add(newUser);
-	   }
-	   
-	   return friends;
-   }
-   
-   @Transactional
-   public Set<User> getAllUsersInfo(){
-	
-	   Set<User> allUsers = new HashSet<>();
-	   User newUser;
-	   for(User u : usersRepository.findAll()){
-		   newUser = new User();
-		   newUser.setId(u.getId());
-		   newUser.setUsername(u.getUsername());
-		   newUser.setFirstName(u.getFirstName());
-		   newUser.setLastName(u.getLastName());
-		   allUsers.add(newUser);
-	   }
-	   
-	   return allUsers;	
-   }
-   
-   public User findUserByUsername(String username){
-	   return usersRepository.findByUsername(username);
-   }
-   
+
+    public boolean isUsernameAvailable(String username) {
+
+        return usersRepository.findByUsername(username) == null;
+    }
+
+    public void save(User user) {
+        usersRepository.save(user);
+    }
+
+
+    //get users with with just username, firstName, lastName field
+    //it is possibly stuped solution for self reference json formatting problem
     @Transactional
-   public Notification addNotificationToUser(String username, String eventType){
-	   User receiver = findUserByUsername(username);
-	   User author = usersRepository.findByUsername(principal.getUsername()); 
-	   
-	   Notification notification = new Notification();
-	   notification.setAuthor(author);
-	   receiver.addNotification(notification);
-	   notification.setEventType(eventType);
-	   
-	   usersRepository.save(receiver);
-	   return notification;
-	   
-   }
-   
-   @Transactional
-   public void removeNotificationFromUser(String username, Long notificationId){
-	   User user = usersRepository.findByUsername(username);
-	   user.getNotifications().removeIf(notification -> notification.getId() == notificationId);
-	   usersRepository.save(user);
-   }
-   
-   @Transactional
-   public void addCurrentUserFollowing(String username){
-	   User currentUser = usersRepository.findByUsername(username);
-	   User following = usersRepository.findByUsername(username);
-	   currentUser.addFollowing(following);
+    public Set<User> getCurrentUserFriends() {
+        User user = usersRepository.findByUsername(principal.getUsername());
+        Set<User> friends = new HashSet<>();
+        User newUser;
+        for (User u : user.getFriends()) {
+            newUser = new User();
+            newUser.setId(u.getId());
+            newUser.setUsername(u.getUsername());
+            newUser.setFirstName(u.getFirstName());
+            newUser.setLastName(u.getLastName());
+            friends.add(newUser);
+        }
 
-	   usersRepository.save(currentUser);
+        return friends;
+    }
 
-   }
-   
-   @Transactional
-   public Set<Notification> getUserNotifications(String username){
-		User user = usersRepository.findByUsername(username);
-		
-		Set<Notification> result = new TreeSet<>(new Comparator<Notification>() {
-			@Override
-			public int compare(Notification o1, Notification o2) {
-				return (int)(o1.getId() - o2.getId());
-			}
-		});
+    @Transactional
+    public Set<User> getUserFriends(String username) {
 
-		for(Notification n: user.getNotifications()){
-			result.add(n);
-		}
-		
-	   return result;
-}
-  
-	@Transactional
-   public Set<Notification> getCurrentUserNotifications(){
-		User user = getCurrentUser();
-		
-		Set<Notification> result = new TreeSet<>(new Comparator<Notification>() {
-			@Override
-			public int compare(Notification o1, Notification o2) {
-				return (int)(o1.getId() - o2.getId());
-			}
-		});
+        User user = usersRepository.findByUsername(username);
+        Set<User> friends = new HashSet<>();
+        User newUser;
+        for (User u : user.getFriends()) {
+            newUser = new User();
+            newUser.setId(u.getId());
+            newUser.setUsername(u.getUsername());
+            newUser.setFirstName(u.getFirstName());
+            newUser.setLastName(u.getLastName());
+            friends.add(newUser);
+        }
+        return friends;
+    }
 
-		for(Notification n: user.getNotifications()){
-			result.add(n);
-		}
-		
-	   return result;
-}
-   
+    @Transactional
+    public Set<User> getAllUsersInfo() {
+
+        Set<User> allUsers = new HashSet<>();
+        User newUser;
+        for (User u : usersRepository.findAll()) {
+            newUser = new User();
+            newUser.setId(u.getId());
+            newUser.setUsername(u.getUsername());
+            newUser.setFirstName(u.getFirstName());
+            newUser.setLastName(u.getLastName());
+            allUsers.add(newUser);
+        }
+
+        return allUsers;
+    }
+
+    public User findUserByUsername(String username) {
+        return usersRepository.findByUsername(username);
+    }
+
+    @Transactional
+    public Notification addNotificationToUser(String username, String eventType) {
+        User receiver = findUserByUsername(username);
+        User author = usersRepository.findByUsername(principal.getUsername());
+
+        Notification notification = new Notification();
+        notification.setAuthor(author);
+        receiver.addNotification(notification);
+        notification.setEventType(eventType);
+
+        usersRepository.save(receiver);
+        return notification;
+    }
+
+    @Transactional
+    public void removeNotificationFromUser(String username, Long notificationId) {
+        User user = usersRepository.findByUsername(username);
+        user.getNotifications().removeIf(notification -> notification.getId().equals(notificationId));
+        usersRepository.save(user);
+    }
+
+    @Transactional
+    public void addCurrentUserFollowing(String username) {
+        User currentUser = usersRepository.findByUsername(username);
+        User following = usersRepository.findByUsername(username);
+        currentUser.addFollowing(following);
+
+        usersRepository.save(currentUser);
+
+    }
+
+    @Transactional
+    public Set<Notification> getUserNotifications(String username) {
+        User user = usersRepository.findByUsername(username);
+
+        Set<Notification> result = new TreeSet<>(new Comparator<Notification>() {
+            @Override
+            public int compare(Notification o1, Notification o2) {
+                return (int) (o1.getId() - o2.getId());
+            }
+        });
+
+        for (Notification n : user.getNotifications()) {
+            result.add(n);
+        }
+
+        return result;
+    }
+
+    @Transactional
+    public Set<Notification> getCurrentUserNotifications() {
+        User user = getCurrentUser();
+
+        Set<Notification> result = new TreeSet<>(new Comparator<Notification>() {
+            @Override
+            public int compare(Notification o1, Notification o2) {
+                return (int) (o1.getId() - o2.getId());
+            }
+        });
+
+        for (Notification n : user.getNotifications()) {
+            result.add(n);
+        }
+
+        return result;
+    }
 }
